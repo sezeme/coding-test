feb5695fcb5fe662fcf47dcef1f3d9dd1c677d1b
sezeme <stellaa223@gmail.com>
2025-12-27 16:07:55 +0900

    TODO 7: 코드 최적화

--- src/main/java/com/seowon/coding/domain/model/PermissionChecker.java
+++ src/main/java/com/seowon/coding/domain/model/PermissionChecker.java
@@ -3,6 +3,7 @@ package com.seowon.coding.domain.model;
 
 import lombok.Builder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class PermissionChecker {
@@ -19,27 +20,37 @@ class PermissionChecker {
             List<UserGroup> groups,
             List<Policy> policies
     ) {
-        for (User user : users) {
-            if (user.id.equals(userId)) {
-                for (String groupId : user.groupIds) {
-                    for (UserGroup group : groups) {
-                        if (group.id.equals(groupId)) {
-                            for (String policyId : group.policyIds) {
-                                for (Policy policy : policies) {
-                                    if (policy.id.equals(policyId)) {
-                                        for (Statement statement : policy.statements) {
-                                            if (statement.actions.contains(targetAction) &&
-                                                statement.resources.contains(targetResource)) {
-                                                return true;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+        // 1. 해당 권한 및 정책을 가지고 있는 policy 필터링
+        // * 해당 policy를 가지고 있는 group 찾기
+        List<Policy> foundPolicies = new ArrayList<>();
+        for (Policy policy : policies) {
+            for (Statement statement : policy.statements) {
+                if (statement.actions.contains(targetAction) &&
+                        statement.resources.contains(targetResource)) {
+                    foundPolicies.add(policy);
                 }
             }
+        }
+        if (foundPolicies.isEmpty()) return false;
+
+        List<UserGroup> foundGroup = new ArrayList<>();
+        for (Policy policy : foundPolicies) {
+            for (UserGroup group : groups) {
+                if (group.policyIds.contains(policy.id)) foundGroup.add(group);
+            }
+        }
+        if (foundGroup.isEmpty()) return false;
+
+        // 2. target user이 있는지 찾기
+        User foundUser = null;
+        for (User user : users) {
+            if (user.id.equals(userId)) foundUser = user;
+        }
+        if (foundUser == null) return false;
+
+        // 3. user가 해당 group에 소속되어있는지 확인
+        for (UserGroup group : foundGroup) {
+            if (foundUser.groupIds.contains(group.id)) return true;
         }
         return false;
     }
